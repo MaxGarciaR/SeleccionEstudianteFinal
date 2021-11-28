356d44ba57b4f78c35e5d91edde51a6cb97e800c
75753325 <75753325@continental.edu.pe>
2021-11-28 18:22:27 -0500

    Configuración de la generacion de datos aleatorios - añadir Estudiante

---
+++ tests/test_añadirEstudiante.py
@@ -0,0 +1,39 @@
+from faker import Faker
+import random
+import unittest
+'''Importar la logica de añadir estudiante'''
+from src.seleccionestudiantefinal.modelo.Estudiante import Estudiante, Elegible
+from src.seleccionestudiantefinal.modelo.declarative_base import Session
+
+class AñadirEstudianteTestCase(unittest.TestCase):
+    def setUp(self):
+        self.AñadirEstudiante = AñadirEstudiante()
+
+        self.session = Session()
+
+        self.data_factory = Faker ()
+
+        self.seed (1000)
+
+        self.data = [ ]
+        self.añaEstudiante=[ ]
+        self.elegible = [Elegible.si, Elegible.no]
+        for i in range (0,10):
+            self.data.append((
+                self.data_factory.unique.name( ),
+                self.data_factory.unique.name( ),
+                self.data_factory.unique.name( ),
+                random.choice (self.elegible)))
+            self.añaEstudiante.append(
+                Estudiante(
+                    apellidoPaterno = self.data[-1][0],
+                    apellidoMaterno = self.data[-1][1],
+                    nombres = self.data[-1][2],
+                    elegibles = self.data[-1][3],
+                    equipos = [],
+                    asignaturas = [],
+                ))
+            self.session.add (self.añaEstudiante[-1])
+
+        self.session.commit()
+
